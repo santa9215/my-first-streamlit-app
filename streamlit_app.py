a93ae92c450ee50a101681a3e202665daa7c0d65
santa9215 <41205299+santa9215@users.noreply.github.com>
2026-01-13 16:41:20 +0000

    second_commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
 import streamlit as st
 
-st.title("Hello Streamlit!")
-st.write("GitHubからデプロイした最初のアプリです。")
+st.title("カスタマイズしたアプリ")
+
+# テキスト入力ボックス
+name = st.text_input("お名前を入力してください")
+
+# ボタン
+if st.button("挨拶する"):
+    st.write(f"こんにちは、{name}さん！Streamlitへようこそ。")
+
+# スライダー
+level = st.slider("今の気分は？", 0, 100, 50)
+st.write(f"現在の気分レベル: {level}")
